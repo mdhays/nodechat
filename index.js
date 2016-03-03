0bec1a0a88f3bb9832d5a12a5c703d1b661836ec
Matt Hays <mdhays1995@gmail.com>
2016-03-03 14:10:24 -0600

    chats route shows the stored messages.

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
-'use strict';
+'use strict'
 
-const express = require('express');
-const app = express();
-const server = require('http').createServer(app);
-const ws = require('socket.io')(server);
+const express = require('express')
+const app = express()
+const pg = require('pg').native
+const server = require('http').createServer(app)
+const ws = require('socket.io')(server)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
+const POSTGRES_URL = process.env.POSTGRES_URL
+  || 'postgres://localhost:5432/nodechat'
 
-app.set('view engine', 'jade');
+let db
 
-app.use(express.static('public'));
+app.set('view engine', 'jade')
+
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index');
-});
+  res.render('index')
+})
 
-server.listen(PORT, () => {
-  console.log(`server listening on port: ${PORT}.`);
-});
+app.get('/chats', (req, res) => {
+  db.query('SELECT * FROM chats', (err, result) => {
+    if (err) throw err
+
+    res.send(result.rows)
+  })
+})
+
+pg.connect(POSTGRES_URL, (err, client) => {
+  if (err) throw err
+
+  db = client
+
+  server.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`)
+  })
+})
 
 ws.on('connection', socket => {
-  console.log('connection', socket);
+  console.log('socket connected')
 
   socket.on('sendChat', msg => {
     console.log(msg)
     socket.broadcast.emit('receiveChat', msg)
   })
-});
+})
+
+
+
